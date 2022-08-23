c0321503778032dbc53bd70c9f8369812e60d760
SOUBHIK BISWAS <sbiswas773@gmail.com>
2022-08-24 01:23:50 +0530

    updateIndexJs

---
+++ js/index.js
@@ -0,0 +1,266 @@
+// ***********************************************
+// ************** Element Selector *********************
+// ***********************************************
+
+//Pizza Button
+const p1Btn = document.getElementById("p1Btn");
+const p2Btn = document.getElementById("p2Btn");
+const p3Btn = document.getElementById("p3Btn");
+const p4Btn = document.getElementById("p4Btn");
+const p5Btn = document.getElementById("p5Btn");
+const p6Btn = document.getElementById("p6Btn");
+const p7Btn = document.getElementById("p7Btn");
+const p8Btn = document.getElementById("p8Btn");
+const p9Btn = document.getElementById("p9Btn");
+//Pizza Button
+
+// Size Button
+const p1sBtn = document.getElementById("p1s");
+const p1mBtn = document.getElementById("p1m");
+const p1lBtn = document.getElementById("p1l");
+
+const p2sBtn = document.getElementById("p2s");
+const p2mBtn = document.getElementById("p2m");
+const p2lBtn = document.getElementById("p2l");
+
+const p3sBtn = document.getElementById("p3s");
+const p3mBtn = document.getElementById("p3m");
+const p3lBtn = document.getElementById("p3l");
+
+const p4sBtn = document.getElementById("p4s");
+const p4mBtn = document.getElementById("p4m");
+const p4lBtn = document.getElementById("p4l");
+
+const p5sBtn = document.getElementById("p5s");
+const p5mBtn = document.getElementById("p5m");
+const p5lBtn = document.getElementById("p5l");
+
+const p6sBtn = document.getElementById("p6s");
+const p6mBtn = document.getElementById("p6m");
+const p6lBtn = document.getElementById("p6l");
+
+const p7sBtn = document.getElementById("p7s");
+const p7mBtn = document.getElementById("p7m");
+const p7lBtn = document.getElementById("p7l");
+
+const p8sBtn = document.getElementById("p8s");
+const p8mBtn = document.getElementById("p8m");
+const p8lBtn = document.getElementById("p8l");
+
+const p9sBtn = document.getElementById("p9s");
+const p9mBtn = document.getElementById("p9m");
+const p9lBtn = document.getElementById("p9l");
+// Size Button
+
+//Size & Price Span:
+const p1Size = document.getElementById("p1si");
+const p2Size = document.getElementById("p2si");
+const p3Size = document.getElementById("p3si");
+const p4Size = document.getElementById("p4si");
+const p5Size = document.getElementById("p5si");
+const p6Size = document.getElementById("p6si");
+const p7Size = document.getElementById("p7si");
+const p8Size = document.getElementById("p8si");
+const p9Size = document.getElementById("p9si");
+
+const p1Price = document.getElementById("p1p");
+const p2Price = document.getElementById("p2p");
+const p3Price = document.getElementById("p3p");
+const p4Price = document.getElementById("p4p");
+const p5Price = document.getElementById("p5p");
+const p6Price = document.getElementById("p6p");
+const p7Price = document.getElementById("p7p");
+const p8Price = document.getElementById("p8p");
+const p9Price = document.getElementById("p9p");
+//Size & Price Span:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ***********************************************
+// **** Size & Price Change Clickz Event *********
+// ***********************************************
+
+
+//Size & Price Change Clickz Event
+p1sBtn.addEventListener("click",()=>{
+    p1Price.textContent="110";
+    p1Size.textContent="Small";
+})
+p1mBtn.addEventListener("click",()=>{
+    p1Price.textContent="160";
+    p1Size.textContent="Medium";
+})
+p1lBtn.addEventListener("click",()=>{
+    p1Price.textContent="200";
+    p1Size.textContent="Large";
+})
+
+
+
+p2sBtn.addEventListener("click",()=>{
+    p2Price.textContent="160";
+    p2Size.textContent="Small";
+})
+p2mBtn.addEventListener("click",()=>{
+    p2Price.textContent="210";
+    p2Size.textContent="Medium";
+})
+p2lBtn.addEventListener("click",()=>{
+    p2Price.textContent="250";
+    p2Size.textContent="Large";
+})
+
+
+
+
+p3sBtn.addEventListener("click",()=>{
+    p3Price.textContent="130";
+    p3Size.textContent="Small";
+})
+p3mBtn.addEventListener("click",()=>{
+    p3Price.textContent="180";
+    p3Size.textContent="Medium";
+})
+p3lBtn.addEventListener("click",()=>{
+    p3Price.textContent="220";
+    p3Size.textContent="Large";
+})
+
+
+
+p4sBtn.addEventListener("click",()=>{
+    p4Price.textContent="120";
+    p4Size.textContent="Small";
+})
+p4mBtn.addEventListener("click",()=>{
+    p4Price.textContent="170";
+    p4Size.textContent="Medium";
+})
+p4lBtn.addEventListener("click",()=>{
+    p4Price.textContent="210";
+    p4Size.textContent="Large";
+})
+
+
+
+p5sBtn.addEventListener("click",()=>{
+    p5Price.textContent="180";
+    p5Size.textContent="Small";
+})
+p5mBtn.addEventListener("click",()=>{
+    p5Price.textContent="230";
+    p5Size.textContent="Medium";
+})
+p5lBtn.addEventListener("click",()=>{
+    p5Price.textContent="270";
+    p5Size.textContent="Large";
+})
+
+
+
+
+p6sBtn.addEventListener("click",()=>{
+    p6Price.textContent="170";
+    p6Size.textContent="Small";
+})
+p6mBtn.addEventListener("click",()=>{
+    p6Price.textContent="220";
+    p6Size.textContent="Medium";
+})
+p6lBtn.addEventListener("click",()=>{
+    p6Price.textContent="260";
+    p6Size.textContent="Large";
+})
+
+
+
+
+p7sBtn.addEventListener("click",()=>{
+    p7Price.textContent="140";
+    p7Size.textContent="Small";
+})
+p7mBtn.addEventListener("click",()=>{
+    p7Price.textContent="190";
+    p7Size.textContent="Medium";
+})
+p7lBtn.addEventListener("click",()=>{
+    p7Price.textContent="230";
+    p7Size.textContent="Large";
+})
+
+
+
+
+p8sBtn.addEventListener("click",()=>{
+    p8Price.textContent="145";
+    p8Size.textContent="Small";
+})
+p8mBtn.addEventListener("click",()=>{
+    p8Price.textContent="195";
+    p8Size.textContent="Medium";
+})
+p8lBtn.addEventListener("click",()=>{
+    p8Price.textContent="235";
+    p8Size.textContent="Large";
+})
+
+
+
+
+p9sBtn.addEventListener("click",()=>{
+    p9Price.textContent="125";
+    p9Size.textContent="Small";
+})
+p9mBtn.addEventListener("click",()=>{
+    p9Price.textContent="175";
+    p9Size.textContent="Medium";
+})
+p9lBtn.addEventListener("click",()=>{
+    p9Price.textContent="215";
+    p9Size.textContent="Large";
+})
+//Size & Price Change Clickz Event
+
+// ***********************************************
+// **** Pizza Button Clickz Event *********
+// ***********************************************
+
+
+
